c98d93e57a4ddd4c249e2b18370d6bda16b62f30
BOT_Axel <axellebot@gmail.com>
2017-11-14 14:39:15 +0100

    [TASK] Refactored
    - Deleted files from repo
    - Refactored main source for OpenGL

---
+++ sources/constants.h
@@ -0,0 +1,4 @@
+#define Pi 3.141592654
+
+#define MMP_WINDOW_LABEL "Maths - Mini Project"
+#define MMP_AUTHORS "Axel LE BOT | Andrew LENC"
--- sources/main.cpp
+++ sources/main.cpp
@@ -1,48 +1,44 @@
-/*********************************************************/
-/*                    openGL.cc                          */
-/*********************************************************/
-/*                                                       */
-/*********************************************************/
+/*********************************************************
+ *                    main.cpp                           *
+ *********************************************************/
 
 #include <stdlib.h>
+#include <stdbool.h>
 #include <fstream>
 #include <iostream>
-// Inclusion des fichiers d'en-tete Glut
-#include <GL/glut.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <jpeglib.h>
-#include <jerror.h>
 #include <math.h>
 #include <string.h>
+
+// Inclusion des fichiers d'en-tete Glut
+#include <vector>
+#include <GL/glut.h>
+#include <jpeglib.h>
+#include <jerror.h>
+
+#include "constants.h"
+#include "Point.h"
+
 using namespace std;
-
-
-#define Pi 3.141592654
 
 double Scal = 36;
 
 double trX = 0.0, trY = 0.0, dist = 0.;//,trZ=0.0
-char presse;
+bool leftClicked;
 int anglex, angley, x, y, xold, yold;
 
-/* Prototype des fonctions */
-void affichage();//  procedure a modifier en fonction de la scene
-void clavier(unsigned char touche, int x, int y);
-
-void reshape(int x, int y);
-
+/* Prototype des fonctions pour OpenGL */
 void idle();
+void display();//  procedure a modifier en fonction de la scene
+void keyboardInput(unsigned char touche, int x, int y);
+void reshape(int x, int y);
+void mouseInput(int bouton, int etat, int x, int y);
+void mouseMotion(int x, int y);
 
-void mouse(int bouton, int etat, int x, int y);
-
-void mousemotion(int x, int y);
-
-//-************************************************************
-//
-//  Procedure avec mise en file des sommets des primitives
-//
-//-***********************************************************
+/****************************************************************
+ **                                                            **
+ **  Procedure avec mise en file des sommets des primitives    **
+ **                                                            **
+ ****************************************************************/
 void init();
 
 int main(int argc, char **argv) {
@@ -51,7 +47,7 @@ int main(int argc, char **argv) {
     glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
     glutInitWindowPosition(0, 0);
     glutInitWindowSize(1000, 1000);
-    glutCreateWindow("RQBC et conique.");
+    glutCreateWindow(MMP_WINDOW_LABEL);
     /* Initialisation d'OpenGL */
     glClearColor(1.0, 1.0, 1.0, 0.0);
     glColor3f(0.0, 0.0, 0.0);
@@ -63,17 +59,17 @@ int main(int argc, char **argv) {
     //glEdgeFlag(GL_FALSE);
     glEdgeFlag(GL_TRUE);
     glLightModeli(GL_LIGHT_MODEL_LOCAL_VIEWER, GL_TRUE);
-    // glEnable(GL_LIGHTING);
+    //glEnable(GL_LIGHTING);
     glDisable(GL_LIGHTING);
 
 
-    /* enregistrement des fonctions de rappel */
+    /* Register OpenGL Function */
     init();
-    glutDisplayFunc(affichage);
-    glutKeyboardFunc(clavier);
+    glutDisplayFunc(display);
     glutReshapeFunc(reshape);
-    glutMouseFunc(mouse);
-    glutMotionFunc(mousemotion);
+    glutKeyboardFunc(keyboardInput);
+    glutMouseFunc(mouseInput);
+    glutMotionFunc(mouseMotion);
 
 
     /* Entree dans la boucle principale glut */
@@ -81,8 +77,36 @@ int main(int argc, char **argv) {
     return 0;
 }
 
+/****************************************************************
+ **                                                            **
+ **                  OpenGL overloading                        **
+ **                                                            **
+ ****************************************************************/
 
-void clavier(unsigned char touche, int x, int y) {
+// fonction permettant d'afficher les objets en utilisant des listes
+void display() {
+    /* effacement de l'image avec la couleur de fond */
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glLoadIdentity();
+
+    glTranslatef(0.0, 0.0, dist);
+    // Pour la 3D
+    // glRotatef(-angley,1.0,0.0,0.0);
+    //  glRotatef(-anglex,0.0,1.0,0.0);
+    // Pour la 2D
+    glRotatef(-anglex + angley, 0.0, 0.0, 1.0);
+    glScalef(Scal, Scal, Scal); // diminution de la vue de la scene
+    glRotatef(180, 0.0, 1.0, 0.0);
+    glRotatef(180, 1.0, 0.0, 0.0);
+    glTranslatef(-trX, trY, 0.);
+    glCallList(1); // appel de la liste numero 1
+    glCallList(2);   // appel de la liste numero 2
+    glFlush();
+    // On echange les buffers
+    glutSwapBuffers();
+}
+
+void keyboardInput(unsigned char touche, int x, int y) {
     switch (touche) {
         case 'q' : /*la touche 'q' permet de quitter le programme */
             exit(0);
@@ -115,7 +139,6 @@ void clavier(unsigned char touche, int x, int y) {
     }
 }
 
-
 void reshape(int x, int y) {
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
@@ -126,20 +149,20 @@ void reshape(int x, int y) {
     glViewport(0, 0, x, y);
 }
 
-void mouse(int button, int state, int x, int y) {
+void mouseInput(int button, int state, int x, int y) {
     /* si on appuie sur le bouton gauche */
     if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
-        presse = 1; /* le booleen presse passe a 1 (vrai) */
+        leftClicked = true; /* le booleen leftClicked passe a 1 (vrai) */
         xold = x; /* on sauvegarde la position de la souris */
         yold = y;
     }
     /* si on relache le bouton gauche */
     if (button == GLUT_LEFT_BUTTON && state == GLUT_UP)
-        presse = 0; /* le booleen presse passe a 0 (faux) */
+        leftClicked = false; /* le booleen leftClicked passe a 0 (faux) */
 }
 
-void mousemotion(int x, int y) {
-    if (presse) /* si le bouton gauche est presse */
+void mouseMotion(int x, int y) {
+    if (leftClicked) /* si le bouton gauche est leftClicked */
     {
         /* on modifie les angles de rotation de l'objet
        en fonction de la position actuelle de la souris et de la derniere
@@ -156,39 +179,67 @@ void mousemotion(int x, int y) {
 
 /****************************************************************
  **                                                            **
- **                    Affichage de  la scene                  **
+ **                           Tracage                          **
  **                                                            **
  ****************************************************************/
 
-void openGL(double x, double y, double r0, double g0, double b0, double size) {
+/**
+ * Tracer un point
+ * @param x Coordinate X
+ * @param y Coordinate Y
+ * @param r0    Color Red
+ * @param g0    Color Green
+ * @param b0    Color Blue
+ * @param size  Size
+ */
+void trace_point(Point &p, double r0, double g0, double b0, double size) {
     glColor3f(r0, g0, b0);    //initialisation de la couleur
     glPointSize(size);    // initialisation de la taille
     glBegin(GL_POINTS);    // on trace un point
-    glVertex2f(x, y);    // coordonnees du point
+    glVertex2f(p.x, p.y);    // coordonnees du point
     glEnd();    // fin de glBegin
 }
 
-void trace_segment(double x0, double y0, double x1, double y1, double red, double green, double blue, double size) {
+/**
+ * Tracer un Segment
+ * @param x0
+ * @param y0
+ * @param x1
+ * @param y1
+ * @param red
+ * @param green
+ * @param blue
+ * @param size
+ */
+void trace_segment(Point &p1, Point &p2, double red, double green, double blue, double size) {
     glColor3f(red, green, blue);//initialisation de la couleur
     glLineWidth(size); // initialisation de la taille
     glBegin(GL_LINES); // on trace un segment
-    glVertex2f(x0, y0); // coordonnees du premier point
-    glVertex2f(x1, y1); // coordonnees du dernier point
+    glVertex2f(p1.x, p1.y); // coordonnees du premier point
+    glVertex2f(p2.x, p2.y); // coordonnees du dernier point
     glEnd(); // fin de glBegin
 }
 
-
+/****************************************************************
+ **                                                            **
+ **                    Affichage de  la scene                  **
+ **                                                            **
+ ****************************************************************/
 //fonction ou les objets sont a definir
 void init() {
-    double xO = 0., yO = 0., xI = 1., yI = 0., xJ = 0., yJ = 1.;
+    cout << "\n Start Initialisation" << endl;
+
+    Point o ,i,j;
+    o.x=0.,o.y=0., i.x=1.,i.y=0.,j.x=0.,j.y=1.;
+
     glNewList(1, GL_COMPILE_AND_EXECUTE); //liste numero 1
-    openGL(xI, yI, 1., 0., 0., 10.); //I
-    openGL(xJ, yJ, 0., 0.5, 0., 10.); //J
-    openGL(xO, yO, 0., 0., 1., 15.);//O
+    trace_point(o, 0., 0., 1., 15.);//O
+    trace_point(i, 1., 0., 0., 10.); //I
+    trace_point(j, 0., 0.5, 0., 10.); //J
     glEndList();
     glNewList(2, GL_COMPILE_AND_EXECUTE);  //liste numero 2
-    trace_segment(xO, yO, xI, yI, 1.0, 0.0, 1.0, 2.0); // on trace [OI]
-    trace_segment(xO, yO, xJ, yJ, 1.0, 0.50, 0.0, 2.0);// on trace [OJ]
+    trace_segment(o,i, 1.0, 0.0, 1.0, 2.0); // on trace [OI]
+    trace_segment(o,j, 1.0, 0.50, 0.0, 2.0);// on trace [OJ]
     glEndList();
 
     glNewList(4, GL_COMPILE_AND_EXECUTE);  //liste numero 4
@@ -202,31 +253,7 @@ void init() {
 
     glEndList();
 
-
-    cout << "\n Voila, c'est fini" << endl;
-
+    cout << "\n End of Initialisation" << endl;
 }
 
-// fonction permettant d'afficher les objets en utilisant des listes   
-void affichage() {
-    /* effacement de l'image avec la couleur de fond */
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glLoadIdentity();
-
-    glTranslatef(0.0, 0.0, dist);
-    // Pour la 3D
-    // glRotatef(-angley,1.0,0.0,0.0);
-    //  glRotatef(-anglex,0.0,1.0,0.0);
-    // Pour la 2D
-    glRotatef(-anglex + angley, 0.0, 0.0, 1.0);
-    glScalef(Scal, Scal, Scal); // diminution de la vue de la scene
-    glRotatef(180, 0.0, 1.0, 0.0);
-    glRotatef(180, 1.0, 0.0, 0.0);
-    glTranslatef(-trX, trY, 0.);
-    glCallList(1); // appel de la liste numero 1
-    glCallList(2);   // appel de la liste numero 2
-    glFlush();
-    // On echange les buffers
-    glutSwapBuffers();
-}
 
